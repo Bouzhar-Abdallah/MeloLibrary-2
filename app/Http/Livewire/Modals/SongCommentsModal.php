b4a15993f88070c59c6789d4fab3bf5ea40129d1
abdallah <abddoullah.bouzhar@gmail.com>
2023-03-26 22:01:17 +0000

    comments adding succesefully

--- app/Http/Livewire/Modals/SongCommentsModal.php
+++ app/Http/Livewire/Modals/SongCommentsModal.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire\Modals;
 
+use App\Models\comment;
 use App\Models\song;
 use Illuminate\Support\Facades\Auth;
 use LivewireUI\Modal\ModalComponent;
@@ -18,14 +19,22 @@ class SongCommentsModal extends ModalComponent
     }
     public function createComment()
     {
-        var_dump($this->newComment);
-        $comment = $this->song->comments()->create([
-            'user_id' => Auth::user()->id,
-            'text' => $this->newComment,
-        ]);
-
-        $this->emit('flashMessage', 'comment created successfully', 'success');
-   
+        $comment = new comment();
+        $comment->user_id = Auth::user()->id;
+        $comment->song_id = $this->song->id;
+        $comment->text = $this->newComment;
+        
+        if ($comment->save()) {
+            $this->emit('flashMessage', 'comment created successfully', 'success');
+        }else {
+            $this->emit('flashMessage', 'something went wrong', 'failure');
+        }
+        $this->closeModal();
+    }
+    public function report(){
+        $this->emit('flashMessage', 'comment reported successfully', 'success');
+        $this->closeModal();
+        
     }
     public function render()
     {
--- resources/views/livewire/modals/song-comments-modal.blade.php
+++ resources/views/livewire/modals/song-comments-modal.blade.php
@@ -19,7 +19,7 @@
 <div class="flex items-start space-x-4">
 
   <div class="min-w-0 flex-1">
-    <form action="#" class="relative">
+    <div class="relative">
       <div class="border border-gray-300 rounded-lg shadow-sm overflow-hidden focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-indigo-500">
         <label for="comment" class="sr-only">Add your comment</label>
         <textarea wire:model="newComment" rows="3" name="comment" id="comment" class="block w-full py-3 border-0 resize-none focus:ring-0 sm:text-sm" 
@@ -37,10 +37,10 @@
       <div class="absolute bottom-0 inset-x-0 pl-3 pr-2 py-2 flex justify-between">
 
         <div class="flex-shrink-0">
-          <button type="submit"  class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Post</button>
+          <button type="submit" wire:click="createComment" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Post</button>
         </div>
       </div>
-    </form>
+    </div>
   </div>
 </div>
 </div>
