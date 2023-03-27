7a710b6bda8b5f8edc3b08ae43f8438295fbe48b
abdallah <abddoullah.bouzhar@gmail.com>
2023-03-27 04:59:12 +0000

    search according

--- app/Http/Livewire/User/SearchBar.php
+++ app/Http/Livewire/User/SearchBar.php
@@ -10,7 +10,15 @@ class SearchBar extends Component
     public $searchString = '';
     public $songs;
     public function updatedSearchString(){
-        $this->songs = song::where('title', 'like', '%' . $this->searchString . '%')->get();
+        $this->songs = song::where('title', 'like', '%' . $this->searchString . '%')
+        ->orWhereHas('genres', function($query) {
+            $query->where('name', 'like', '%' . $this->searchString . '%');
+        })
+        ->orWhereHas('artists', function($query) {
+            $query->where('name', 'like', '%' . $this->searchString . '%');
+        })
+        ->orWhere('lyrics', 'like', '%' . $this->searchString . '%')
+        ->get();
     }
     public function render()
     {
